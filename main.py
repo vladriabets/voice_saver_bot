e94de384f50cb8aebcdbb487c687d395394d4126
vladriabets <vladriabets@yandex.ru>
2019-05-24 19:58:17 +0300

    create directory named by uid if not exists and add inti it a new audiofile with next number

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ from telepot.loop import MessageLoop
 TOKEN = os.environ.get('TOKEN')
 bot = telepot.Bot(TOKEN)
 
+def add_new_record(file, user_id):
+    dir_name = str(user_id)
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+    number = str(len(os.listdir(dir_name)))
+    with open(dir_name + '/audio_message_' + number + '.oga', 'wb') as voice_message:
+        voice_message.write(file.content)
+
+
 def on_chat_message(msg):
     """Обработчик сообщений"""
 
@@ -21,8 +30,7 @@ def on_chat_message(msg):
         print(file_info)
         file = requests.get('https://api.telegram.org/file/bot{0}/{1}'
         .format(TOKEN, file_info['file_path']))
-        with open('voice.oga', 'wb') as voice_message:
-            voice_message.write(file.content)
+        add_new_record(file, user_id)
 
         print(file)
 
